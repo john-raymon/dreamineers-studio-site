f2fde4e0b31ca9e53a2f88c45d90257793326c16
John Ray <john@trychec.com>
2021-04-02 02:05:54 -0400

    set up svgr/webpack loader for svg components, set-up tailwindcss and home page

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config;
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sync': ['Syncopate', 'sans-serif'],
+    },
+    extend: {
+      maxHeight: {
+        "49rem": '49rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
